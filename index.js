853502e3109129e968e160c6dc8f4299f5f22547
gaurangtorvekar <gaurangtorvekar@gmail.com>
2018-07-15 19:45:13 +0800

    Parses a file

---
+++ index.js
@@ -0,0 +1,28 @@
+var sleep = require('sleep');
+var Tx = require('ethereumjs-tx');
+var Web3 = require('web3');
+var fs = require('fs'); 
+var parse = require('csv-parse');
+var path = require('path');
+
+var _to = [];
+var _value = [];
+
+// var fullFileName = 'give_1.csv';
+var fullFileName = process.argv.slice(2)[0];
+console.log("========= File name = ", fullFileName)
+fs.createReadStream(path.resolve(__dirname, fullFileName))
+    .pipe(parse({
+        delimiter: ','
+    }))
+    .on('data', function(csvrow) {
+        _to.push(csvrow[0]);
+        _value.push(csvrow[1] * 1000000000000000000);
+    })
+    .on('end', function() {
+        //do something wiht csvData
+        console.log(_to);
+        console.log(_value);
+        _to = [];
+        _value = [];
+    });
