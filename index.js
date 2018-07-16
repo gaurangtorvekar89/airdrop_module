6a77f51792716a00bdcc4fb7b981af964c269c03
gaurang89 <gaurangtorvekar89@gmail.com>
2018-07-16 09:47:21 +0800

    Test commit

--- index.js
+++ index.js
@@ -8,27 +8,23 @@ var path = require('path');
 var _to = [];
 var _value = [];
 
-// var fullFileName = 'give_1.csv';
-var fullFileName = process.argv.slice(2)[0];
-console.log("========= File name = ", fullFileName)
-fs.createReadStream(path.resolve(__dirname, fullFileName))
-    .pipe(parse({
-        delimiter: ','
-    }))
-    .on('data', function(csvrow) {
-        _to.push(csvrow[0]);
-        _value.push(csvrow[1] * 1000000000000000000);
-    })
-    .on('end', function() {
-        //do something wiht csvData
-        console.log(_to);
-        console.log(_value);
-        _to = [];
-        _value = [];
-    });
-// module.exports = {
-//     tester: function(){
-//         console.log("In the tester function");
-//         return "1";
-//     }
-// }
+module.exports = function(){
+    // var fullFileName = 'give_1.csv';
+    var fullFileName = process.argv.slice(2)[0];
+    console.log("========= File name = ", fullFileName)
+    fs.createReadStream(path.resolve(__dirname, fullFileName))
+        .pipe(parse({
+            delimiter: ','
+        }))
+        .on('data', function(csvrow) {
+            _to.push(csvrow[0]);
+            _value.push(csvrow[1] * 1000000000000000000);
+        })
+        .on('end', function() {
+            //do something wiht csvData
+            console.log(_to);
+            console.log(_value);
+            _to = [];
+            _value = [];
+        });
+}
--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var tester = require('../index');
 
 describe('#tester', function(){
 	it ('should console.log', function(){
-		var result = tester.tester(1);
-		expect(result).to.equal('1');
+		var result = tester();
+		
 	});
 });
