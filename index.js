67808db6f9ac7437a9d3262520645445922b9fec
gaurang89 <gaurangtorvekar89@gmail.com>
2018-07-16 10:05:27 +0800

    Test commit

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var path = require('path');
 
 var _to = [];
 var _value = [];
-var fullFileName = process.argv.slice(2)[0];
+
+module.exports = function(){
+    var fullFileName = process.argv.slice(2)[0];
     console.log("========= File name = ", fullFileName)
     fs.createReadStream(path.resolve(process.cwd(), fullFileName))
         .pipe(parse({
@@ -18,27 +20,6 @@ var fullFileName = process.argv.slice(2)[0];
             _value.push(csvrow[1] * 1000000000000000000);
         })
         .on('end', function() {
-            //do something wiht csvData
-            console.log(_to);
-            console.log(_value);
-            _to = [];
-            _value = [];
-        });
-
-module.exports = function(){
-    // var fullFileName = 'give_1.csv';
-    var fullFileName = process.argv.slice(2)[0];
-    console.log("========= File name = ", fullFileName)
-    fs.createReadStream(path.resolve(__dirname, fullFileName))
-        .pipe(parse({
-            delimiter: ','
-        }))
-        .on('data', function(csvrow) {
-            _to.push(csvrow[0]);
-            _value.push(csvrow[1] * 1000000000000000000);
-        })
-        .on('end', function() {
-            //do something wiht csvData
             console.log(_to);
             console.log(_value);
             _to = [];
